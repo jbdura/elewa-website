b5d1547158fe68bbdbdaed09f530785961310dd7
jbdura <bidurajohnson@gmail.com>
2023-09-14 16:50:25 +0300

    Created component

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -6,13 +6,13 @@ import { CarouselModule } from '@elewa-website/elements/layout/carousel';
 import { MainPageModule } from '@elewa-website/elements/layout/pages/main-page';
 import { BannersModule } from '@elewa-website/elements/banners';
 
-
 import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
 import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 
 import { HomeRoutingModule } from './home.routing';
 import { OverviewSectionComponent } from './components/overview-section/overview-section.component';
+import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 
 @NgModule({
   imports: [
@@ -28,6 +28,7 @@ import { OverviewSectionComponent } from './components/overview-section/overview
     NewsSectionComponent,
     ProjectsSectionComponent,
     OverviewSectionComponent,
+    HomeHeroSectionComponent,
   ],
   exports: [HomePageComponent],
 })
