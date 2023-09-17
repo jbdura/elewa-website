c143d52a435518f50aa2fe109fa7efd27d60e8c6
jbdura <bidurajohnson@gmail.com>
2023-09-17 11:47:10 +0300

    fixed all bugs

--- libs/data/sections/src/lib/home-hero-section.data.ts
+++ libs/data/sections/src/lib/home-hero-section.data.ts
@@ -1,20 +1,19 @@
-import { ButtonData, SliderButtonData } from "@elewa-website/models/schema/ui/buttons";
+import { SliderButtonData } from "@elewa-website/models/schema/ui/buttons";
 import { HeroText } from "@elewa-website/models/schema/ui/texts";
 import { ImageConfig, ImageVisualisation } from '@elewa-website/models/schema/ui/images';
 
 
 //Wording of the button
-export const __homeButtonData: SliderButtonData[] = [ {
+export const __homeButtonData: SliderButtonData=  {
   text: 'Unlock your potential ',
-} ];
+};
 
 // Wording of the text section
-export const __homeHerotexts: HeroText[] = [
+export const __homeHerotexts: HeroText = 
   {
     title: "Training at scale",
     subtitle: `Human-centered, meaningful and enjoyable`
-  }
-];
+  };
 
 // Add the imageConfig property
 export const __homeImageConfig: ImageConfig =
--- libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
+++ libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
@@ -1,15 +1,5 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'elewa-website-home-hero-section',
-//   templateUrl: './home-hero-section.component.html',
-//   styleUrls: ['./home-hero-section.component.scss'],
-// })
-// export class HomeHeroSectionComponent {}
-
 import { Component } from '@angular/core';
 import { __homeButtonData, __homeHerotexts, __homeImageConfig } from '@elewa-website/data/sections';
-
 
 @Component({
   selector: 'elewa-website-home-hero-section',
